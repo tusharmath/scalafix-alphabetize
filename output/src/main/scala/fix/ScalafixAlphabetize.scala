0c2cbc2cea3f4542c75d54c42cc226f7a2c06c91
Brice Jaglin <bjaglin@gmail.com>
2022-02-11 10:05:50 +0100

    showcase usage of doc.comments.leading

--- output/src/main/scala/fix/ScalafixAlphabetize.scala
+++ output/src/main/scala/fix/ScalafixAlphabetize.scala
@@ -2,25 +2,49 @@ package fix
 
 object ScalafixAlphabetize {
   trait T1 {
-    def t1a: Unit
-    def t1b: Unit
+    
+    
+
+def t1a: Unit
+
+def t1b: Unit
   }
 
   trait T2 {
     def t2x: Unit
   }
 
-  def a = ()
-  def b = ()
-
-  private def c = ()
-
-  def e = new T2 {
+  
+  
+  
+  
+/**
+    * this is a
+    */
+def a = ()
+// this is b
+def b = ()
+/* this is c */
+private def c = ()
+/** this is e */
+def e = new T2 {
     override def t2x: Unit = ()
   }
 
+  
+  
+
+  
+  
+
+  /** this is d
+    */
   val d = new T1 {
-    override def t1a: Unit = ()
-    override def t1b: Unit = ()
+    
+    
+
+override def t1a: Unit = ()
+
+override def t1b: Unit = ()
   }
 }
--- rules/src/main/scala/fix/ScalafixAlphabetize.scala
+++ rules/src/main/scala/fix/ScalafixAlphabetize.scala
@@ -4,7 +4,7 @@ import scalafix.v1._
 import scala.meta._
 
 class ScalafixAlphabetize extends SemanticRule("ScalafixAlphabetize") {
-  def patchTemplate(t: Template): Patch = {
+  def patchTemplate(t: Template)(implicit doc: SemanticDocument): Patch = {
     val defDefinition = t.stats.collect({
       case (d: Defn.Def) => (d, d.name.value)
       case (d: Decl.Def) => (d, d.name.value)
@@ -13,10 +13,17 @@ class ScalafixAlphabetize extends SemanticRule("ScalafixAlphabetize") {
     if (defDefinition.length <= 1) return Patch.empty
 
     val unsortedDefs = defDefinition.map(_._1)
-    val sortedDefs   = defDefinition.sortBy(_._2).map(_._1)
+    val removes      = unsortedDefs.map { tree =>
+      Patch.removeTokens(doc.comments.leading(tree) ++ tree.tokens)
+    }
 
-    (unsortedDefs zip sortedDefs).map({ case (t1, t2) => Patch.replaceTree(t1, t2.syntax) }).asPatch
-  }
+    val sortedDefs = defDefinition.sortBy(_._2).map(_._1)
+    val syntax     = sortedDefs.foldLeft("") { case (acc, tree) =>
+      Seq(acc, doc.comments.leading(tree).map(_.syntax).mkString, tree.syntax).mkString("\n")
+    }
+
+    (removes :+ Patch.addLeft(sortedDefs.head, syntax)).asPatch
+  } 
 
   override def fix(implicit doc: SemanticDocument): Patch = {
     doc.tree
